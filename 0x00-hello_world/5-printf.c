a42270a7789faa374599d81de8558e408dfde167
Sagemanuel <mashapaemmanuel@outlook.com>
2023-02-20 14:21:58 -0800

    testfile

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints entry
+ *
+ * return: always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	/*return: always 0 (Success) */
+
+	return (0);
+}
