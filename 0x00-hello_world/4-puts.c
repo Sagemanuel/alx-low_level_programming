e53a222cfe451fcb903f100dfe867d5a44e2138f
Sagemanuel <mashapaemmanuel@outlook.com>
2023-02-20 13:57:12 -0800

    hello world project

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - puts
+ *
  * return: always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
